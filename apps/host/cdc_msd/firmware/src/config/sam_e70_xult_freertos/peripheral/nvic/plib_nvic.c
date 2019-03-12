dcd2a71888b10e82346943657d973ad246bf64e9
Nagaraju Sreeram <nagaraju.sreeram@microchip.com>
2019-03-12 12:46:32 +0530

    MH3-7853 demo regenerated and tested

--- apps/host/cdc_msd/firmware/src/config/sam_e70_xult_freertos/peripheral/nvic/plib_nvic.c
+++ apps/host/cdc_msd/firmware/src/config/sam_e70_xult_freertos/peripheral/nvic/plib_nvic.c
@@ -62,6 +62,8 @@ void NVIC_Initialize( void )
     NVIC_SetPriority(SysTick_IRQn, 7);
     NVIC_SetPriority(TC0_CH0_IRQn, 7);
     NVIC_EnableIRQ(TC0_CH0_IRQn);
+    NVIC_SetPriority(USBHS_IRQn, 7);
+    NVIC_EnableIRQ(USBHS_IRQn);
 
 
 
