02af127ec6fc3e348d47e608caee84b513551a84
Sundarakumar R <sundarakumar.r@microchip.com>
2019-07-05 10:58:45 +0530

    [MH3-17164]: SAME70 - Device - hid_mouse - FS/HS - Passed.

--- apps/device/hid_mouse/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/device/hid_mouse/firmware/src/config/sam_e70_xult/definitions.h
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "bsp/bsp.h"
+#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "usb/usb_device_hid.h"
 #include "usb/usb_hid.h"
 #include "peripheral/clk/plib_clk.h"
@@ -57,8 +57,10 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
+#include "peripheral/efc/plib_efc.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
+#include "osal/osal.h"
 #include "app.h"
 
 
