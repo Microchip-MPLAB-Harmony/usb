9acb889697a1e43feb8ec437218876dcebb98eae
Jean Christophe Buda <jeanchristophe.buda@microchip.com>
2019-09-19 14:55:54 +0200

    MH3-22169 Regenerate and test apps\host\hid_basic_keyboard\sam_d21_xpro.X demo

--- apps/host/hid_basic_keyboard/firmware/src/config/sam_d21_xpro/configuration.h
+++ apps/host/hid_basic_keyboard/firmware/src/config/sam_d21_xpro/configuration.h
@@ -163,6 +163,9 @@ extern "C" {
 /* Reset duration in milli Seconds */ 
 #define DRV_USBFSV1_HOST_RESET_DURATION                     100
 
+/* Alignment for buffers that are submitted to USB Driver*/ 
+#define USB_ALIGN  CACHE_ALIGN
+
 /* Number of HID Client driver instances in the application */
 #define USB_HOST_HID_INSTANCES_NUMBER        1
 
