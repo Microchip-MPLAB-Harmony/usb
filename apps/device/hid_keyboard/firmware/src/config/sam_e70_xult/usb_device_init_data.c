2d71f25dbdd88d5bca516e5926042303bb1c7add
Shijas Mayan <shijas.mayan@microchip.com>
2020-02-07 15:46:29 +0530

    [APPS]: Regenerate apps\device\hid_keyboard\sam_e70_xult.X demo MH3-29575

--- apps/device/hid_keyboard/firmware/src/config/sam_e70_xult/usb_device_init_data.c
+++ apps/device/hid_keyboard/firmware/src/config/sam_e70_xult/usb_device_init_data.c
@@ -129,6 +129,7 @@ const USB_DEVICE_FUNCTION_REGISTRATION_TABLE funcRegistrationTable[1] =
 /*******************************************
  *  USB Device Descriptor 
  *******************************************/
+
 const USB_DEVICE_DESCRIPTOR deviceDescriptor =
 {
     0x12,                                                   // Size of this descriptor in bytes
@@ -353,7 +354,7 @@ USB_DEVICE_CONFIGURATION_DESCRIPTORS_TABLE fullSpeedConfigDescSet[1] =
 /*******************************************
  *  Product string descriptor
  *******************************************/
-    const struct
+	const struct
     {
         uint8_t bLength;                                    // Size of this descriptor in bytes
         uint8_t bDscType;                                   // STRING descriptor type
@@ -378,6 +379,7 @@ USB_DEVICE_STRING_DESCRIPTORS_TABLE stringDescriptors[3]=
 /*******************************************
  * USB Device Layer Master Descriptor Table 
  *******************************************/
+ 
 const USB_DEVICE_MASTER_DESCRIPTOR usbMasterDescriptor =
 {
     &deviceDescriptor,                                      // Full speed descriptor
@@ -396,6 +398,7 @@ const USB_DEVICE_MASTER_DESCRIPTOR usbMasterDescriptor =
 /****************************************************
  * USB Device Layer Initialization Data
  ****************************************************/
+
 const USB_DEVICE_INIT usbDevInitData =
 {
     /* Number of function drivers registered to this instance of the
