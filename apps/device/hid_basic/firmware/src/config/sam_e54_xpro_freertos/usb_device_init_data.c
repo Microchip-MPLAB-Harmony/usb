15be8b99f30b43df6350f4dcea092f16ad52042f
Sundarakumar R <sundarakumar.r@microchip.com>
2020-02-10 09:14:55 +0530

    [APPS]: Regenerating apps\device\hid_basic\sam_e54_xpro_freertos.X demo [MH3-29567].

--- apps/device/hid_basic/firmware/src/config/sam_e54_xpro_freertos/usb_device_init_data.c
+++ apps/device/hid_basic/firmware/src/config/sam_e54_xpro_freertos/usb_device_init_data.c
@@ -111,6 +111,7 @@ const USB_DEVICE_FUNCTION_REGISTRATION_TABLE funcRegistrationTable[1] =
 /*******************************************
  *  USB Device Descriptor 
  *******************************************/
+
 const USB_DEVICE_DESCRIPTOR deviceDescriptor =
 {
     0x12,                                                   // Size of this descriptor in bytes
@@ -244,7 +245,7 @@ USB_DEVICE_CONFIGURATION_DESCRIPTORS_TABLE fullSpeedConfigDescSet[1] =
 /*******************************************
  *  Product string descriptor
  *******************************************/
-    const struct
+	const struct
     {
         uint8_t bLength;                                    // Size of this descriptor in bytes
         uint8_t bDscType;                                   // STRING descriptor type
@@ -269,6 +270,7 @@ USB_DEVICE_STRING_DESCRIPTORS_TABLE stringDescriptors[3]=
 /*******************************************
  * USB Device Layer Master Descriptor Table 
  *******************************************/
+ 
 const USB_DEVICE_MASTER_DESCRIPTOR usbMasterDescriptor =
 {
     &deviceDescriptor,                                      // Full speed descriptor
@@ -287,6 +289,7 @@ const USB_DEVICE_MASTER_DESCRIPTOR usbMasterDescriptor =
 /****************************************************
  * USB Device Layer Initialization Data
  ****************************************************/
+
 const USB_DEVICE_INIT usbDevInitData =
 {
     /* Number of function drivers registered to this instance of the
