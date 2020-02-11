22a24d1f63f0f074538be646640f3ce85cea5dc2
Shijas Mayan <shijas.mayan@microchip.com>
2020-02-11 17:23:49 +0530

    [APPS]: Regenerated apps\device\hid_mouse\sam_e70_xult_freertos.X demo. MH3-29578

--- apps/device/hid_mouse/firmware/src/config/sam_e70_xult_freertos/tasks.c
+++ apps/device/hid_mouse/firmware/src/config/sam_e70_xult_freertos/tasks.c
@@ -65,7 +65,7 @@ void _DRV_USBHSV1_Tasks(  void *pvParameters  )
     {
 				 /* USB HS Driver Task Routine */
         DRV_USBHSV1_Tasks(sysObj.drvUSBHSV1Object);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
 
@@ -75,7 +75,7 @@ void _USB_DEVICE_Tasks(  void *pvParameters  )
     {
 				 /* USB Device layer tasks routine */
         USB_DEVICE_Tasks(sysObj.usbDevObject0);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
 
@@ -87,7 +87,7 @@ void _APP_Tasks(  void *pvParameters  )
     while(1)
     {
         APP_Tasks();
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
 
