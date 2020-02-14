49c0235c4d0f29a78eda1a6e54562d2df733264b
nagaraju sreeram <nagaraju.sreeram@microchip.com>
2020-02-14 15:08:02 +0530

    [APPS]: modified mhc clock   apps\host\msd_basic\sam_e70_xult.X  demo MH3-29627

--- apps/host/msd_basic/firmware/src/config/sam_e70_xult_xc32/peripheral/clk/plib_clk.c
+++ apps/host/msd_basic/firmware/src/config/sam_e70_xult_xc32/peripheral/clk/plib_clk.c
@@ -34,17 +34,14 @@ Initialize Main Clock (MAINCK)
 *********************************************************************************/
 static void CLK_MainClockInitialize(void)
 {
-    /* Enable Main Crystal Oscillator */
-    PMC_REGS->CKGR_MOR = (PMC_REGS->CKGR_MOR & ~CKGR_MOR_MOSCXTST_Msk) | CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCXTST(255) | CKGR_MOR_MOSCXTEN_Msk;
+    /* Disable Main Crystal Oscillator and Enable External Clock Signal on XIN pin  */
+    PMC_REGS->CKGR_MOR = (PMC_REGS->CKGR_MOR & ~CKGR_MOR_MOSCXTEN_Msk) | CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCXTBY_Msk;
 
-    /* Wait until the main oscillator clock is ready */
-    while ( (PMC_REGS->PMC_SR & PMC_SR_MOSCXTS_Msk) != PMC_SR_MOSCXTS_Msk);
+     /* External clock signal (XIN pin) is selected as the Main Clock (MAINCK) source.
+        Switch Main Clock (MAINCK) to External signal on XIN pin */
+    PMC_REGS->CKGR_MOR |= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCSEL_Msk;
 
-    /* Main Crystal Oscillator is selected as the Main Clock (MAINCK) source.
-    Switch Main Clock (MAINCK) to Main Crystal Oscillator clock */
-    PMC_REGS->CKGR_MOR|= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCSEL_Msk;
-
-    /* Wait until MAINCK is switched to Main Crystal Oscillator */
+    /* Wait until MAINCK is switched to External Clock Signal (XIN pin) */
     while ( (PMC_REGS->PMC_SR & PMC_SR_MOSCSELS_Msk) != PMC_SR_MOSCSELS_Msk);
 
 
