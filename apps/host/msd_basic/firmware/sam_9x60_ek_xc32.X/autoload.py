b0283fcb7d12807269fde9ced0584e8d62bcd2d6
Sundarakumar R <sundarakumar.r@microchip.com>
2020-04-01 08:44:14 +0530

    [APPS]: Deleting demos to re-create them [MH3-31119][MH3-30412].

--- apps/host/msd_basic/firmware/sam_9x60_ek_xc32.X/autoload.py
+++
@@ -1,5 +0,0 @@
-global g_mpu_config
-g_mpu_config = {'type':'application',
-                'entry_addr':0x23f00000,
-                'bootstrap_entry_addr':0x300000,
-                'bootstrap_bin_path':'boot.bin'}
