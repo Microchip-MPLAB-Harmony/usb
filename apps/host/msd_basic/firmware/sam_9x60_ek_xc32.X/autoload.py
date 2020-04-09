76511fd08781ea3c3a024f27b1f3d9269d9229ba
Sundarakumar R <sundarakumar.r@microchip.com>
2020-04-09 07:17:42 +0530

    [APPS]: Restoring SAM9X60 EK XC32 demo [MH3-31119][MH3-30412].

---
+++ apps/host/msd_basic/firmware/sam_9x60_ek_xc32.X/autoload.py
@@ -0,0 +1,5 @@
+global g_mpu_config
+g_mpu_config = {'type':'application',
+                'entry_addr':0x23f00000,
+                'bootstrap_entry_addr':0x300000,
+                'bootstrap_bin_path':'boot.bin'}
