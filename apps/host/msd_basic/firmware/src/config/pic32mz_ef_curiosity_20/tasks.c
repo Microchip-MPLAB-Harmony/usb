68ec7ec9dc6c24658e8a9a2347f415dbff522457
Sunil Fernandes <sunil.fernandes@microchip.com>
2020-06-17 13:33:50 +0530

    [PIC32MZ USB Host MSD Demo Apps]: Regenerated demo apps to for updates in file system component. [MH3-37005]

--- apps/host/msd_basic/firmware/src/config/pic32mz_ef_curiosity_20/tasks.c
+++ apps/host/msd_basic/firmware/src/config/pic32mz_ef_curiosity_20/tasks.c
@@ -55,6 +55,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -91,6 +92,7 @@ SYS_FS_Tasks();
     /* Maintain the application's state machine. */
         /* Call Application task APP. */
     APP_Tasks();
+
 
 
 
