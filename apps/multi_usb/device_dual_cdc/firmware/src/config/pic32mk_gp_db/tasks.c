ca3d1d0e5d94515927c661f55853f330b0252eb6
Sundarakumar R <sundarakumar.r@microchip.com>
2020-06-25 11:44:15 +0530

    [APPS]: Regeneration of demo multi_usb/device_dual_cdc/firmware/pic32mk_gp_db.X [MH3-37295].
    Signed-off-by: Sundarakumar R <sundarakumar.r@microchip.com>

--- apps/multi_usb/device_dual_cdc/firmware/src/config/pic32mk_gp_db/tasks.c
+++ apps/multi_usb/device_dual_cdc/firmware/src/config/pic32mk_gp_db/tasks.c
@@ -55,6 +55,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -94,6 +95,7 @@ void SYS_Tasks ( void )
     /* Maintain the application's state machine. */
         /* Call Application task APP. */
     APP_Tasks();
+
 
 
 
