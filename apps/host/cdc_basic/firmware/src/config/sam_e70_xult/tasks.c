633400d166674d0a5980c4c8310b5a062d6a3f57
Sundarakumar R <sundarakumar.r@microchip.com>
2020-06-25 10:46:30 +0530

    [APPS]: Regeneration of demo host/cdc_basic/firmware/sam_e70_xult.X [MH3-37244].
    Signed-off-by: Sundarakumar R <sundarakumar.r@microchip.com>

--- apps/host/cdc_basic/firmware/src/config/sam_e70_xult/tasks.c
+++ apps/host/cdc_basic/firmware/src/config/sam_e70_xult/tasks.c
@@ -55,6 +55,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -88,6 +89,7 @@ void SYS_Tasks ( void )
     /* Maintain the application's state machine. */
         /* Call Application task APP. */
     APP_Tasks();
+
 
 
 
