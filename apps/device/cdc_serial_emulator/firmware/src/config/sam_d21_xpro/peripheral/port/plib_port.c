9f0749ea26e04c324bfdbdab747b211eee70dc17
Sundarakumar R <sundarakumar.r@microchip.com>
2020-06-25 09:52:33 +0530

    [APPS]: Regeneration of demo device/cdc_serial_emulator/firmware/sam_d21_xpro.X [MH3-37194].
    Signed-off-by: Sundarakumar R <sundarakumar.r@microchip.com>

--- apps/device/cdc_serial_emulator/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
+++ apps/device/cdc_serial_emulator/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
@@ -82,10 +82,12 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
    PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x66;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
    PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
 
+                
 }
 
 // *****************************************************************************
