bcaa15e011888f5187808443024cd0d9db9a320d
Shijas Mayan <shijas.mayan@microchip.com>
2020-07-13 17:08:27 +0530

    [APPS]: Increased push button delay device\cdc_com_port_single\firmware\sam_9x60_ek_iar.IAR [MH3-37175]

--- apps/device/cdc_com_port_single/firmware/src/config/sam_9x60_ek_iar/user.h
+++ apps/device/cdc_com_port_single/firmware/src/config/sam_9x60_ek_iar/user.h
@@ -32,8 +32,8 @@ extern "C" {
 // Section: User Configuration macros
 // *****************************************************************************
 // *****************************************************************************
-#define APP_USB_SWITCH_DEBOUNCE_COUNT_FS                    75
-#define APP_USB_SWITCH_DEBOUNCE_COUNT_HS                    100
+#define APP_USB_SWITCH_DEBOUNCE_COUNT_FS                    150
+#define APP_USB_SWITCH_DEBOUNCE_COUNT_HS                    150
 
 
 //DOM-IGNORE-BEGIN
