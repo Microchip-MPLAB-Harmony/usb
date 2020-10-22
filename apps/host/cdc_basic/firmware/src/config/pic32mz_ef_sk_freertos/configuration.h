a71bdc6dfdd03f30ac83adf4158cb97410cb0cd5
Sundarakumar R <sundarakumar.r@microchip.com>
2020-10-22 23:48:22 +0530

    [APPS]: Regeneration of demo host/cdc_basic/firmware/pic32mz_ef_sk_freertos.X [MH3-45361].
    Signed-off-by: Sundarakumar R <sundarakumar.r@microchip.com>

--- apps/host/cdc_basic/firmware/src/config/pic32mz_ef_sk_freertos/configuration.h
+++ apps/host/cdc_basic/firmware/src/config/pic32mz_ef_sk_freertos/configuration.h
@@ -146,9 +146,6 @@ extern "C" {
 /* Total number of devices to be supported */
 #define USB_HOST_DEVICES_NUMBER                             1
 
-/* Size of Endpoint 0 buffer */
-#define USB_DEVICE_EP0_BUFFER_SIZE                          64
-
 /* Target peripheral list entries */
 #define  USB_HOST_TPL_ENTRIES                               1 
 
@@ -162,8 +159,6 @@ extern "C" {
 /* Provides Host pipes number */
 #define USB_HOST_PIPES_NUMBER                               10
 
-/* Number of Host Layer Clients */
-#define USB_HOST_CLIENTS_NUMBER                             1   
 
 /* Number of CDC Function driver instances in the application */
 #define USB_HOST_CDC_INSTANCES_NUMBER         1
