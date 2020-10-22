b61cad61d938607eb09f7a4b09cf992ebdbb2414
Sundarakumar R <sundarakumar.r@microchip.com>
2020-10-22 23:09:57 +0530

    [APPS]: Regeneration of demo device/cdc_com_port_single/firmware/sam_g55_xpro.X [MH3-45310].
    Signed-off-by: Sundarakumar R <sundarakumar.r@microchip.com>

--- apps/device/cdc_com_port_single/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.h
+++ apps/device/cdc_com_port_single/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.h
@@ -66,9 +66,9 @@
 #define USB_VBUS_SENSE_Set()               (PIOB_REGS->PIO_SODR = (1<<4))
 #define USB_VBUS_SENSE_Clear()             (PIOB_REGS->PIO_CODR = (1<<4))
 #define USB_VBUS_SENSE_Toggle()            (PIOB_REGS->PIO_ODSR ^= (1<<4))
-#define USB_VBUS_SENSE_Get()               ((PIOB_REGS->PIO_PDSR >> 4) & 0x1)
 #define USB_VBUS_SENSE_OutputEnable()      (PIOB_REGS->PIO_OER = (1<<4))
 #define USB_VBUS_SENSE_InputEnable()       (PIOB_REGS->PIO_ODR = (1<<4))
+#define USB_VBUS_SENSE_Get()               ((PIOB_REGS->PIO_PDSR >> 4) & 0x1)
 #define USB_VBUS_SENSE_PIN                  PIO_PIN_PB4
 
 
