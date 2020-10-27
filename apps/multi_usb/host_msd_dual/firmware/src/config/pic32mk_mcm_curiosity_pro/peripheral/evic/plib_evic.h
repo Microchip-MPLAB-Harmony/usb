af81bb81f9a551db78fa4e433769f1d1f683619d
Shijas Mayan <shijas.mayan@microchip.com>
2020-10-27 14:05:18 +0530

    [Apps]: Regenerated and tested the demo apps\multi_usb\host_msd_dual\firmware\pic32mk_mcm_curiosity_pro.X [MH3-45426]

--- apps/multi_usb/host_msd_dual/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/evic/plib_evic.h
+++ apps/multi_usb/host_msd_dual/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/evic/plib_evic.h
@@ -61,6 +61,8 @@
 #endif
 // DOM-IGNORE-END
 
+
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Data Types
@@ -531,6 +533,7 @@ typedef enum
 
 } INT_SOURCE;
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Interface Routines
@@ -550,6 +553,7 @@ bool EVIC_SourceStatusGet( INT_SOURCE source );
 void EVIC_SourceStatusSet( INT_SOURCE source );
 
 void EVIC_SourceStatusClear( INT_SOURCE source );
+
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
