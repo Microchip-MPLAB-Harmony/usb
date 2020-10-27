56da81768102bae5e04284f3b227d91ac118bd20
nagaraju sreeram <nagaraju.sreeram@microchip.com>
2020-10-27 10:10:48 +0530

    [APPS] Regenerated Demo host\msd_basic\firmware\sam_d21_xpro.X [MH3-45410]

--- apps/host/msd_basic/firmware/src/config/sam_d21_xpro/configuration.h
+++ apps/host/msd_basic/firmware/src/config/sam_d21_xpro/configuration.h
@@ -153,9 +153,6 @@ extern "C" {
 /* Total number of devices to be supported */
 #define USB_HOST_DEVICES_NUMBER                             1
 
-/* Size of Endpoint 0 buffer */
-#define USB_DEVICE_EP0_BUFFER_SIZE                          64
-
 /* Target peripheral list entries */
 #define  USB_HOST_TPL_ENTRIES                               1 
 
@@ -169,8 +166,6 @@ extern "C" {
 /* Provides Host pipes number */
 #define USB_HOST_PIPES_NUMBER                               10
 
-/* Number of Host Layer Clients */
-#define USB_HOST_CLIENTS_NUMBER                             1   
 
 /*** USB Driver Configuration ***/
 
--- apps/host/msd_basic/firmware/src/config/sam_d21_xpro/device.h
+++ apps/host/msd_basic/firmware/src/config/sam_d21_xpro/device.h
@@ -1,20 +1,22 @@
 /*******************************************************************************
-  USB stack external dependencies file
+  Device Header File
 
   Company:
     Microchip Technology Inc.
 
   File Name:
-    drv_usb_external_dependencies.h
+    device.h
 
   Summary:
-    USB Driver external dependencies file
+    This file includes the selected device from within the project.
+    The device will provide access to respective device packs.
 
   Description:
-    USB Driver external dependencies file. 
+    None
+
 *******************************************************************************/
 
-//DOM-IGNORE-BEGIN
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
 * Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
 *
@@ -36,33 +38,18 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *******************************************************************************/
-//DOM-IGNORE-END
+*******************************************************************************/
+// DOM-IGNORE-END
 
-#ifndef _DRV_USB_EXTERNAL_DEPENDENCIES_H
-#define _DRV_USB_EXTERNAL_DEPENDENCIES_H
+#pragma GCC diagnostic push
+#ifndef __cplusplus
+#pragma GCC diagnostic ignored "-Wnested-externs"
+#endif
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wattributes"
+#pragma GCC diagnostic ignored "-Wundef"
+#include "atsamd21j18a.h"
+#pragma GCC diagnostic pop
+#include "device_cache.h"
+#include "toolchain_specifics.h"
 
-#include <string.h>
-#include "system/system_common.h"
-#include "configuration.h"
-#include "definitions.h"
-#include "system/system_module.h"
-
-#if  !defined(SYS_DEBUG_ENABLE) 
-
-    #if  !defined(SYS_DEBUG_PRINT)
-        #define SYS_DEBUG_PRINT(level, format, ...) 
-    #endif
-
-    #if  !defined(SYS_DEBUG_MESSAGE)
-        #define SYS_DEBUG_MESSAGE(a,b, ...)   
-    #endif
-
-    #if  !defined(SYS_DEBUG)
-        #define SYS_DEBUG(a,b)
-    #endif 
-#endif 
-#endif /* End of #ifndef _DRV_USB_EXTERNAL_DEPENDENCIES_H */ 
-/*******************************************************************************
- End of File
-*/
