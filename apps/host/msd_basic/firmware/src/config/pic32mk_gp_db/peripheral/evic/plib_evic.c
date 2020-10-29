d6c9a4436ddecbad863b7f3569a34dcbd5be51ef
Shijas Mayan <shijas.mayan@microchip.com>
2020-10-29 09:14:40 +0530

    [APPS]: Regenerated and tested the demo apps\host\msd_basic\firmware\pic32mk_gp_db.X [MH3-45396]

--- apps/host/msd_basic/firmware/src/config/pic32mk_gp_db/peripheral/evic/plib_evic.c
+++ apps/host/msd_basic/firmware/src/config/pic32mk_gp_db/peripheral/evic/plib_evic.c
@@ -43,6 +43,7 @@
 #include "device.h"
 #include "plib_evic.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: IRQ Implementation
@@ -53,9 +54,14 @@ void EVIC_Initialize( void )
 {
     INTCONSET = _INTCON_MVEC_MASK;
 
-    /* Set up priority / subpriority of enabled interrupts */
+    /* Set up priority and subpriority of enabled interrupts */
     IPC0SET = 0x4 | 0x0;  /* CORE_TIMER:  Priority 1 / Subpriority 0 */
     IPC8SET = 0x40000 | 0x0;  /* USB_1:  Priority 1 / Subpriority 0 */
+
+
+
+    /* Configure Shadow Register Set */
+    PRISS = 0x10000000;
 }
 
 void EVIC_SourceEnable( INT_SOURCE source )
@@ -104,3 +110,5 @@ void EVIC_SourceStatusClear( INT_SOURCE source )
     *IFSxCLR = 1 << (source & 0x1f);
 }
 
+
+/* End of file */
