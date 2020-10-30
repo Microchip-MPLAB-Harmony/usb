c7d254b03e6ee86b8dd4d015186d98a6d8dce6ab
nagaraju sreeram <nagaraju.sreeram@microchip.com>
2020-10-30 14:08:32 +0530

    [APPS] Regenerated demo host\msd_basic\firmware\sam_a5d2_xult_freertos_iar.IAR [MH3-45408]

--- apps/host/msd_basic/firmware/src/config/sam_a5d2_xult_freertos_iar/configuration.h
+++ apps/host/msd_basic/firmware/src/config/sam_a5d2_xult_freertos_iar/configuration.h
@@ -80,13 +80,13 @@ extern "C" {
 // *****************************************************************************
 // *****************************************************************************
 /* TIME System Service Configuration Options */
-#define SYS_TIME_INDEX_0                     0
-#define SYS_TIME_MAX_TIMERS                  5
-#define SYS_TIME_HW_COUNTER_WIDTH            32
-#define SYS_TIME_HW_COUNTER_PERIOD           4294967295U
-#define SYS_TIME_HW_COUNTER_HALF_PERIOD	     (SYS_TIME_HW_COUNTER_PERIOD>>1)
-#define SYS_TIME_CPU_CLOCK_FREQUENCY         498000000
-#define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES      (2200)
+#define SYS_TIME_INDEX_0                            (0)
+#define SYS_TIME_MAX_TIMERS                         (5)
+#define SYS_TIME_HW_COUNTER_WIDTH                   (32)
+#define SYS_TIME_HW_COUNTER_PERIOD                  (4294967295U)
+#define SYS_TIME_HW_COUNTER_HALF_PERIOD             (SYS_TIME_HW_COUNTER_PERIOD>>1)
+#define SYS_TIME_CPU_CLOCK_FREQUENCY                (498000000)
+#define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES    (2200)
 
 
 /* File System Service Configuration */
@@ -112,6 +112,7 @@ extern "C" {
 #define SYS_FS_FAT_READONLY               false
 #define SYS_FS_FAT_CODE_PAGE              437
 #define SYS_FS_FAT_MAX_SS                 SYS_FS_MEDIA_MAX_BLOCK_SIZE
+#define SYS_FS_FAT_ALIGNED_BUFFER_LEN     512
 
 
 
@@ -144,28 +145,43 @@ extern "C" {
 #define USB_HOST_MSD_LUN_NUMBERS              1
 
 
-/*** USB Driver Configuration ***/
+	
+/*** USB EHCI Driver Configurations ***/
 
 /* Maximum USB driver instances */
-#define DRV_USB_UHP_INSTANCES_NUMBER                         1
-
-/* Interrupt mode enabled */
-#define DRV_USB_UHP_INTERRUPT_MODE                           true
-
-/* Number of NAKs to wait before returning transfer failure */ 
-#define DRV_USB_UHP_NAK_LIMIT                          2000 
-
-/* Maximum Number of pipes */
-#define DRV_USB_UHP_PIPES_NUMBER                       10  
+#define DRV_USB_EHCI_INSTANCES_NUMBER                     1
 
 /* Attach Debounce duration in milli Seconds */ 
-#define DRV_USB_UHP_ATTACH_DEBOUNCE_DURATION           500
+#define DRV_USB_EHCI_ATTACH_DEBOUNCE_DURATION           500
 
 /* Reset duration in milli Seconds */ 
-#define DRV_USB_UHP_RESET_DURATION                     100
+#define DRV_USB_EHCI_RESET_DURATION                     100
 
-/* Maximum Transfer Size */ 
-#define DRV_USB_UHP_NO_CACHE_BUFFER_LENGTH  4096
+/* Maximum Control Transfer Size */
+#define DRV_USB_EHCI_CONTROL_TRANSFER_BUFFER_SIZE 512
+
+/* Maximum Non Control Transfer Size */ 
+#define DRV_USB_EHCI_TRANSFER_BUFFER_SIZE  512
+
+	
+
+
+/*** USB OHCI Driver Configurations ***/
+
+#define DRV_USB_OHCI_INSTANCES_NUMBER                        1
+
+/* Attach Debounce duration in milli Seconds */ 
+#define DRV_USB_OHCI_ATTACH_DEBOUNCE_DURATION           500
+
+/* Reset duration in milli Seconds */ 
+#define DRV_USB_OHCI_RESET_DURATION                     100
+
+/* Maximum Control Transfer Size */
+#define DRV_USB_OHCI_CONTROL_TRANSFER_BUFFER_SIZE 512
+
+/* Maximum Non Control Transfer Size */ 
+#define DRV_USB_OHCI_TRANSFER_BUFFER_SIZE  512
+
 
 /* Alignment for buffers that are submitted to USB Driver*/ 
 #ifndef USB_ALIGN
@@ -179,13 +195,9 @@ extern "C" {
 // **************************************************************************
 
 /* Number of Endpoints used */
-#define DRV_USB_UHP_ENDPOINTS_NUMBER                        1
 
 /* Total number of devices to be supported */
 #define USB_HOST_DEVICES_NUMBER                             1
-
-/* Size of Endpoint 0 buffer */
-#define USB_DEVICE_EP0_BUFFER_SIZE                          64
 
 /* Target peripheral list entries */
 #define  USB_HOST_TPL_ENTRIES                               1 
@@ -193,15 +205,13 @@ extern "C" {
 /* Maximum number of configurations supported per device */
 #define USB_HOST_DEVICE_INTERFACES_NUMBER                   5    
 
-#define USB_HOST_CONTROLLERS_NUMBER                         1
+#define USB_HOST_CONTROLLERS_NUMBER                         2
 
 #define USB_HOST_TRANSFERS_NUMBER                           10
 
 /* Provides Host pipes number */
 #define USB_HOST_PIPES_NUMBER                               10
 
-/* Number of Host Layer Clients */
-#define USB_HOST_CLIENTS_NUMBER                             1   
 
 
 
